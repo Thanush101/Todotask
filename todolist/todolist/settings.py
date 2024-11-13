e22e1cccc45c5d12efb453019817c87974be8296
Thanush <115856620+Thanush101@users.noreply.github.com>
2024-11-13 22:57:07 +0530

    Update settings.py

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -62,7 +62,9 @@ ROOT_URLCONF = 'todolist.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',  # Ensure this points to the correct templates directory
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
